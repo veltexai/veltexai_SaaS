6e9bbc65fd1bba7931ef7abb67f6d84aa537da41
Mohamed Chouati <92956374+mc-expertise@users.noreply.github.com>
2025-08-21 20:03:54 +0100

    refactor(config): simplify domainName configuration

--- config/config.ts
+++ config/config.ts
@@ -1,10 +1,7 @@
 const config = {
   appName: 'Veltex Services Veliz',
   appDescription: 'Veltex Services Veliz is a platform for Generate PDF AI.',
-  domainName:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'https://veltexservicesveliz.com',
+  domainName: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
 };
 
 export default config;
