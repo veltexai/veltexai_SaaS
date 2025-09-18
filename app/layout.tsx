f173b545d3f7d9e75df6e23180bc6c1c349b6a6e
Mohamed Chouati <92956374+mc-expertise@users.noreply.github.com>
2025-09-18 09:59:43 +0100

    Add Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from 'sonner';
 import { ConfirmationProvider } from '@/components/providers/confirmation-provider';
+import dynamic from 'next/dynamic';
+const FacebookPixel = dynamic(import('../components/FacebookPixel'));
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <FacebookPixel />
         <ConfirmationProvider>
           {children}
           <Toaster />
